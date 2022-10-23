622de758ab82b87e40e1afc401153c12de844e82
togo-mentor <uint2435@gmail.com>
2022-10-23 09:29:59 +0900

    vpc created

--- lib/cdk-demo-stack.ts
+++ lib/cdk-demo-stack.ts
@@ -1,5 +1,6 @@
-import * as cdk from 'aws-cdk-lib';
-import { Construct } from 'constructs';
+import * as cdk from "aws-cdk-lib";
+import { SubnetType, Vpc } from "aws-cdk-lib/aws-ec2";
+import { Construct } from "constructs";
 // import * as sqs from 'aws-cdk-lib/aws-sqs';
 
 export class CdkDemoStack extends cdk.Stack {
@@ -7,10 +8,25 @@ export class CdkDemoStack extends cdk.Stack {
     super(scope, id, props);
 
     // The code that defines your stack goes here
+    // CHANGE: We have created the vpc object from the Vpc class.
+    const vpc = new Vpc(this, "MainVpc", {
+      // CHANGE: this is where we define how many AZs to use
+      maxAzs: 2,
 
-    // example resource
-    // const queue = new sqs.Queue(this, 'CdkDemoQueue', {
-    //   visibilityTimeout: cdk.Duration.seconds(300)
-    // });
+      // CHANGE: We define a single subnet configuration per AZ.
+      subnetConfiguration: [
+        {
+          // CHANGE: this is it's CIDR mask so 255.255.255.0
+          cidrMask: 24,
+
+          // CHANGE: a name for each of these subnets
+          name: "public-subnet",
+
+          // CHANGE: and the subnet type to be used - here we will have
+          // a public subnet. There are other options available here.
+          subnetType: SubnetType.PUBLIC,
+        },
+      ],
+    });
   }
 }
